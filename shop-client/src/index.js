4cb4a22be013e2445abe8ddf9b0e2a14385076e2
rofviram <57324866+rofviram@users.noreply.github.com>
2020-07-30 10:44:33 +0300

    added spaces

--- shop-client/src/index.js
+++ shop-client/src/index.js
@@ -4,6 +4,9 @@ import App from './components/App'
 import { Router } from 'react-router-dom'
 import history from './history'
 
+
+
+
 ReactDOM.render((
    <Router history={history}>
       <App />
